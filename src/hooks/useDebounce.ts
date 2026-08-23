80c891df0aa8eadb524b575dc9dd195a583f03b7
Ulisesrod <ulisesrod92@gmail.com>
2026-08-23 15:40:45 -0600

    update var in debounce to avoid "any" type

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-export function useDebounce (value, delay){
-    const [debounced, setDebounced] = useState(value)
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
     useEffect(()=>{
-        const handler = setTimeout(()=> setDebounced(value), delay);
+        const handler = setTimeout(()=> setDebouncedValue(value), delay);
         return ()=>clearTimeout(handler);
     }, [value,delay]);
-    return debounced;
+    return debouncedValue;
 }
